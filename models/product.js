10bb3ad65405aa19f353eee2443961e37252e17e
Elliott Wilder <elliottjwilder@gmail.com>
2020-12-14 15:42:03 -0600

    add farm schema

--- models/product.js
+++ models/product.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const productSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required:true
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    category: {
-        type: String, 
-        lowercase: true,
-        enum: ['fruit', 'vegetable', 'dairy']
-    }
-})
+const productSchema = new Schema({
+	name     : {
+		type     : String,
+		required : true
+	},
+	price    : {
+		type     : Number,
+		required : true,
+		min      : 0
+	},
+	category : {
+		type      : String,
+		lowercase : true,
+		enum      : [
+			'fruit',
+			'vegetable',
+			'dairy'
+		]
+	},
+	farm     : {
+		type : Schema.Types.ObjectId,
+		ref  : 'Farm'
+	}
+});
 
 const Product = mongoose.model('Product', productSchema);
 
